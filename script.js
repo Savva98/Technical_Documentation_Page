d7eaf8bcf5c19a61d13faec5d0ec5c4d681b12a5
Savva8030 <93856194+Savva98@users.noreply.github.com>
2023-08-25 13:47:19 +0300

    Delete some text

--- script.js
+++ script.js
@@ -3,10 +3,7 @@ document
   .addEventListener("click", function (e) {
     e.preventDefault();
     if(e.target.classList.contains(`nav-link`)){
-        console.log(e.target);
         const id = e.target.getAttribute('href');
         document.querySelector(id).scrollIntoView({behavior:'smooth'})
     }
   });
-
-  console.log(`hey`);
